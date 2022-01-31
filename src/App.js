437cf72df4d647bfb4a7eb4d95b1971ac1461e88
Ankur Seth <iankurseth@gmail.com>
2022-01-31 22:20:59 +0530

    c0, tailwind and cleanup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="p-8 bg-rose-900">
+				<h2 className="text-4xl text-white text-center">codeshot</h2>
+			</header>
+		</div>
+	);
+}
+
+export default App;
